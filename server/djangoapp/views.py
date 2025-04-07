02e81fa76197149cdeedfab17410fed9642c31aa
dharasurti09 <dharasurti52@gmail.com>
2025-04-07 10:02:11 -0400

    Adding temporary changes to Github

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -16,6 +16,7 @@ from django.contrib import messages
 from datetime import datetime
 from django.http import JsonResponse
 from django.contrib.auth import login, authenticate
+from .models import CarMake, CarModel
 import logging
 import json
 from django.views.decorators.csrf import csrf_exempt
@@ -88,6 +89,17 @@ def registration(request):
     else:
         data = {"username": username, "error": "Already Registered"}
         return JsonResponse(data)
+
+def get_cars(request):
+    count = CarMake.objects.filter().count()
+    print(count)
+    if(count == 0):
+        initiate()
+    car_models = CarModel.objects.select_related('car_make')
+    cars = []
+    for car_model in car_models:
+        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+    return JsonResponse({"CarModels":cars})
 
 # # Update the `get_dealerships` view to render the index page with
 # a list of dealerships
